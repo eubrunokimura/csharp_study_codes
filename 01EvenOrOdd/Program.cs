c3d6ae9544ad9b9672110d2badcb7f054fe22319
Bruno Kimura <eubrunokimura@gmail.com>
2023-10-04 23:40:36 -0400

    Project 01 Even or Odd Created to study convertion types.

---
+++ 01EvenOrOdd/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace EvenOrOdd
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            bool conversion;
+            int result;
+
+
+            do
+            {
+                Console.WriteLine("Even or odd?");
+                string input = Console.ReadLine();
+                conversion = int.TryParse(input, out result);
+            } while (!conversion);
+
+            if (result % 2 == 0)
+            {
+                Console.WriteLine("Even");
+            }
+            else
+            {
+                Console.WriteLine("Odd");
+            }
+
+
+
+        }
+    }
+}
