f9800577202c828513313fcc9d0bcc6b82dfdf2d
Bruno Kimura <eubrunokimura@gmail.com>
2023-10-09 23:29:55 -0400

    Even in Range

---
+++ 02EvenInRange/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace EvenInRange
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Program: Even in Range");
+            Console.WriteLine("That program says even numbers starting from 0 to given number.\n");
+
+            String input;
+            bool validateNumber;
+            int rangeNumber;
+
+            do
+            {
+                Console.WriteLine("Give a number greater than 0: ");
+                input = Console.ReadLine();
+
+                validateNumber = int.TryParse(input, out rangeNumber);
+
+
+                if (!validateNumber || rangeNumber <= 0)
+                {
+                    Console.WriteLine("Invalid Number...");
+                }
+
+
+
+            } while ((!validateNumber) || (rangeNumber <= 0));
+
+
+            Console.WriteLine("\n\n\nRange...");
+            for (int i = 0; i <= rangeNumber; i++)
+            {
+                Console.WriteLine("Number: " + i.ToString() + (i % 2 == 0 ? " -> Even" : String.Empty));
+            }
+
+        }
+    }
+}
---
+++ 02EvenInRangeV2/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace EvenInRange
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Program: Even in Range v2.");
+            Console.WriteLine("In that code we show even number to from 0 to a given input. (Using LINQ)");
+
+
+            bool isNumber;
+            int givenRange;
+            string input;
+
+            do
+            {
+                Console.WriteLine("Write a number: ");
+                input = Console.ReadLine();
+                isNumber = int.TryParse(input, out givenRange);
+
+            } while (!isNumber);
+
+
+
+            IEnumerable<int> numbers = Enumerable.Range(0, givenRange + 1).Where(x => x % 2 == 0);
+            foreach (int i in numbers)
+            {
+                Console.WriteLine(i);
+            }
+
+
+        }
+    }
+}
